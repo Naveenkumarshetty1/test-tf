fcdd875c4397ff58254dbe5f2931140502e62c70
Naveenkumar Shetty <68023294+Naveenkumarshetty1@users.noreply.github.com>
2023-03-03 15:18:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,12 @@
 provider "aws" {
-  region = "us-east-1"  
+  region = "us-east-1"
   access_key = "AKIAS5IM336JPS7DFQ2D"
   secret_key = "j39Bc6WKzRPEdfg0nb34M1BE5O7basAPhPntA0ZC"
-
 }
 
-resource "aws_instance" 
-  ami           = "ami-0557a15b87f6559cf" # specify your desired AMI
-  instance_type = "t2.micro" # specify your desired instance type
-
-  # specify your desired security group
-  security_groups = ["sg-01fb2d2c1c3628ae1"]
-
-  tags = {
-    Name = "new instance"
-  }
+resource "aws_instance" "example" {
+  ami = "ami-006dcf34c09e50022"
+  instance_type = "t2.micro"
+  key_name = "my-key-pair"
+  security_groups = ["default"]
 }
