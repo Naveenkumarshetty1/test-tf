a971f73dda816d816580da3e7951b1c3aeb13017
Naveenkumar Shetty <68023294+Naveenkumarshetty1@users.noreply.github.com>
2023-03-03 15:34:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
   secret_key = "j39Bc6WKzRPEdfg0nb34M1BE5O7basAPhPntA0ZC"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" {
   ami = "ami-006dcf34c09e50022"
   instance_type = "t2.micro"
   security_groups = ["default"]
