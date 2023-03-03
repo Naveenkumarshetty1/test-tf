483b3a6a9829c2476177160fd95cdd2dbfaa6b31
Naveenkumar Shetty <68023294+Naveenkumarshetty1@users.noreply.github.com>
2023-03-03 15:31:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,5 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-006dcf34c09e50022"
   instance_type = "t2.micro"
-  key_name = ["ansible1.pem"]
   security_groups = ["default"]
 }
